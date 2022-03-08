5169472884d75af3b0b806089562d49f9eff93e7
Lildhansen <madslh@yahoo.com>
2022-03-08 19:49:46 +0100

    initial setup for excel

--- Excel.py
+++ Excel.py
@@ -4,12 +4,13 @@ import utilities.util as util
 
 #imports
 import openpyxl
-import xlsxwriter
+#import xlsxwriter
 
 class Excel:
     def __init__(self,players=None):
-        self.players = players
-        self.__getPlayers()
+        self.players = []
+        if players == None:
+            self.__getPlayers()
     def __getPlayers(self):#for testing purposes
         file = open(r"./logs/PlayerAndTeams.txt","r",encoding="utf-8")
         for line in file.readlines():
@@ -18,5 +19,14 @@ class Excel:
             else:
                 strippedSplitLine = util.removeInvalidLetters(line.rstrip()).split(",")
                 self.players[-1].addTeam(strippedSplitLine)
-
+    def setupExcelFile(self):
+        wb = openpyxl.Workbook() # new workbook
+        ws = wb.active #new worksheet
+        ws.title = "Feriekasse"
         
+        #...
+        wb.save("Feriekasse.xlsx")
+        wb.close()
+
+myExcel = Excel()
+myExcel.setupExcelFile()
