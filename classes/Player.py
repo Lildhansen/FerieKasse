52a5913c30d17d6cf09cb71cbe2c202be796c579
Lildhansen <madslh@yahoo.com>
2022-04-16 12:50:11 +0200

    added more league Tests

--- classes/Player.py
+++ classes/Player.py
@@ -2,20 +2,7 @@
 class Player:
     def __init__(self,name,teams):
         self.name = name
-        self.teams = teams 
-        self.menu = None
-    #how teams are to be added - by appending to the list
-    def addTeam(self,team):
-        self.teams.append(team)
-    #adds players and teams to the .txt-file with players, teams and URLs
-    def addToPlayersTeamsAndLinksFile(self):
-        file = open(r"./logs/playersTeamsAndLinks.txt","a+")
-        file.write(f"{self.name}:\n")
-        for team in self.teams:
-            file.write(f"{team.name},{team.url}\n")
-        file.close()
-    def updateTeamPoints(self):
-        for team in self.teams:
-            team.updatePointsForMatch()
+        self.teams = teams
+        self.points = []
 
 
