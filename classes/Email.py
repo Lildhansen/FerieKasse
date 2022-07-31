126f5c7ceaf92f6bd740238c33ff79dfbbd21ec7
lildhansen <madslh@yahoo.com>
2022-07-31 15:52:53 +0200

    cleanup for initial mail

--- classes/Email.py
+++ classes/Email.py
@@ -30,28 +30,36 @@ class Email:
         self.port = int(config.getint(configSection,'port'))
         self.receivers = config.get(configSection,'receivers').split(';')
     def sendInitialEmail(self):
-        mailBody = "The feriekasse has been created and the teams picked by each player is visible in the attached excel file (.xlsx)."
         message = EmailMessage()
-        message['From'] = self.sender
-        message['To'] = self.receivers
-        message['Subject'] = f"feriekassen, {const.FERIEKASSE_NAME}, has been created"
-        #add body
-        message.set_content(mailBody)
+        mailBody = "The feriekasse has been created and the teams picked by each player is visible in the attached excel file (.xlsx)."
+        subject = f"feriekassen, {const.FERIEKASSE_NAME}, has been created"
+        
+        self.setupMailInfo(message,self.sender,self.receivers,subject,mailBody)
         
         #attach the excel file
         excelFile = fr"data/{const.FERIEKASSE_NAME}/Feriekasse.xlsx" 
         filename = "feriekasse (" + date.today().strftime("%d-%m-%Y") + ").xlsx"
+        self.attachExcelFile(message,excelFile,filename)
+        
+        self.connectToSmtpAndSendMail(message)
+    def setupMailInfo(self,message,sender,receivers,subject,body):
+        message['From'] = sender
+        message['To'] = receivers
+        message['Subject'] = subject
+        message.set_content(body)
+    
+    def sendMail(self):
+        mailBody = f"Attached is an excel file (.xlsx) with the current standings of the feriekasse."
+    
+    def attachExcelFile(self,message,excelFile,newFileName):
         with open(excelFile, 'rb') as f:
             file_data = f.read()
-        message.add_attachment(file_data, maintype="application", subtype="xlsx", filename=filename)
-        
-        #connect and send mail
+        message.add_attachment(file_data, maintype="application", subtype="xlsx", filename=newFileName)
+
+    def connectToSmtpAndSendMail(self,message):
         smtp = smtplib.SMTP_SSL(self.server)
         smtp.connect(self.server,self.port)
         smtp.login(self.sender,self.password)
         smtp.sendmail(to_addrs=self.receivers,msg=message.as_string(),from_addr=self.sender)
         smtp.quit()
-    def sendMail(self):
-        mailBody = f"Attached is an excel file (.xlsx) with the current standings of the feriekasse."
-
 
