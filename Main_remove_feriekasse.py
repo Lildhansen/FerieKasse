9b5330b7f487bd700efd5ceee81ca3cb48cac661
lildhansen <madslh@yahoo.com>
2022-09-02 21:22:33 +0200

    cleaned up main remove and made consistency between quit/exit (always quit)

--- Main_remove_feriekasse.py
+++ Main_remove_feriekasse.py
@@ -9,7 +9,7 @@ def removeFeriekasse(feriekasseDir):
     feriekasseName = feriekasseDir.split("/")[-1]
     print(f"Feriekasse, {feriekasseName}, has been removed")
     
-def resetFeriekasse():
+def removeFeriekasse():
     print("removing a feriekasse")
     userInput = ""
     while userInput == ""  or userInput.lower() == "-l":
@@ -19,38 +19,34 @@ def resetFeriekasse():
     if userInput == "-a":
         prompt = ""
         while (prompt != "y" and prompt != "n"):
-            prompt = input("are you sure you want to reset all feriekasser? (y/n) ")
+            prompt = input("are you sure you want to remove all feriekasser? (y/n) ")
         if (prompt == "y"):
-            dataDir = fr"./data"
-            for subdir, dirs, files in os.walk(dataDir):
+            for subdir, dirs, files in os.walk("./data"):
                 for dir in dirs:
-                    removeFeriekasse(fr"{dataDir}/{dir}")
+                    removeFeriekasse(fr"./data/{dir}")
             print("All feriekasser has been removed")
-        exit()
+        quit()
     if userInput == "n":
         print("cancelled")
-        exit()
+        quit()
     
     if "," in userInput:
         feriekasser = helperMain.handleMultipleArgumentsForFeriekasser(userInput)
         for feriekasse in feriekasser:
             removeFeriekasse(fr"./data/{feriekasse}")
         print("The selected feriekasser has been removed")
-        exit()
+        quit()
         
     const.FERIEKASSE_NAME = userInput
     feriekasseDir = fr"./data/{const.FERIEKASSE_NAME}"
     if not os.path.exists(feriekasseDir):
         print("This feriekasse does not exist")
-        exit()
+        quit()
     prompt = ""
     while (prompt != "y" and prompt != "n"):
-        prompt = input("are you sure you want to reset this feriekasse? (y/n) ")
+        prompt = input("are you sure you want to remove this feriekasse? (y/n) ")
     if (prompt == "y"):
-        removeFeriekasse(feriekasseDir)
-
-    
-        
+        removeFeriekasse(feriekasseDir)   
 
 if __name__ == "__main__":
-    resetFeriekasse()
+    removeFeriekasse()
