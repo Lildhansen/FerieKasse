9200b7d44332fa0003437ddadddacc977c85f674
lildhansen <madslh@yahoo.com>
2022-10-04 22:59:12 +0200

    prettified test - and made ini files absolute paths when passed to func

--- Testing/Email_test.py
+++ Testing/Email_test.py
@@ -5,9 +5,11 @@ from email.message import EmailMessage
 
 from classes.Email import Email
 
+iniFileLocation = os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'EmailTest.ini'))
+
 def test_data_is_setup_correct_from_ini_file():
     print((os.path.join(os.path.dirname(__file__)),"EmailTestMocks",'EmailTest.ini'))
-    email = Email(os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'EmailTest.ini'))
+    email = Email(iniFileLocation)
     assert email.sender == "me"
     assert email.password == "pword"
     assert email.server == "192.168.0.1"
@@ -18,7 +20,7 @@ def test_data_is_setup_correct_from_ini_file():
     assert email.receivers[1] == "b"
 
 def test_setupMailInfo_sets_up_correct_info_for_message():
-    email = Email(os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'EmailTest.ini'))
+    email = Email(iniFileLocation)
     message = EmailMessage()
     email.mailBody = "this is the body"
     email.subject = "this is the subject"
@@ -30,34 +32,33 @@ def test_setupMailInfo_sets_up_correct_info_for_message():
    
 
 def test_attachExcelFile_adds_excel_file_to_message():
-    email = Email(os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'EmailTest.ini'))
+    email = Email(iniFileLocation)
     message = EmailMessage()
     email.attachExcelFile(message,os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'Feriekasse.xlsx'),"feriekasse.xlsx")
     assert len(message.get_payload()) == 1
-    assert message.get_payload()[0].get_content_type() == "application\\xlsx"
+    assert message.get_payload()[0].get_content_type() == "application/xlsx"
     assert message.get_payload()[0].get_filename() == "feriekasse.xlsx"
 
 def test_attachExcelFileScreenshot_adds_image_to_message():
-    email = Email(os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'EmailTest.ini'))
+    email = Email(iniFileLocation)
     message = EmailMessage()
     email.attachExcelFileScreenshot(message,os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'Feriekasse.xlsx'),'Feriekasse.png')
     assert len(message.get_payload()) == 1
-    assert message.get_payload()[0].get_content_type() == "image\\png" 
+    assert message.get_payload()[0].get_content_type() == "image/png" 
     assert os.path.basename(message.get_payload()[0].get_filename()) == "Feriekasse.png"
  
 #integration test 
 def test_attachFiles_adds_excel_file_and_image_to_message():
-    email = Email(os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'EmailTest.ini'))
+    email = Email(iniFileLocation)
     message = EmailMessage()
     email.attachFiles(message,os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'Feriekasse.xlsx'),"feriekasse.xlsx",os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'Feriekasse.png'))
     assert len(message.get_payload()) == 2
-    assert message.get_payload()[0].get_content_type() == "application\\xlsx"
+    assert message.get_payload()[0].get_content_type() == "application/xlsx"
     assert message.get_payload()[0].get_filename() == "feriekasse.xlsx"
-    assert message.get_payload()[1].get_content_type() == "image\\png" 
+    assert message.get_payload()[1].get_content_type() == "image/png" 
     assert os.path.basename(message.get_payload()[1].get_filename()) == "Feriekasse.png" #for some reason this is the full directory - though it still works fine
     
 def test_updateLastMailSentValue_updates_last_mail_sent_value_to_todays_date_in_ini_file():
-    iniFileLocation = os.path.join(os.path.join(os.path.dirname(__file__),"EmailTestMocks"),'EmailTest.ini')
     email = Email(iniFileLocation)
     config = configparser.ConfigParser()
     
